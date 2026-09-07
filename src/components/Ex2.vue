a15c5d95a4bea5027fc468f1af85a9088effe3cc
daryledwin2025-smu <daryl.edwin.2025@smu.edu.sg>
2026-09-07 09:21:07 +0800

    2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+let data = "/assets/me.png";
 
 // Add code here
 </script>
 
 <template>
+  <!-- // do this using dictionary. and also add the height and width -->
   <!-- Add code here -->
+   <img v-bind:src="data" alt="">
 </template>
